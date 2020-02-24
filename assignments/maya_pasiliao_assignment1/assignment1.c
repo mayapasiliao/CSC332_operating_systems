5f5a6effda58c789c2323b7e13daa92f6ce8c95e
Maya Pasiliao <mayapasiliao@gmail.com>
2020-02-23 20:33:42 -0500

    Finished assignment1 and added assignment description

--- assignments/maya_pasiliao_assignment1/assignment1.c
+++ assignments/maya_pasiliao_assignment1/assignment1.c
@@ -1,5 +1,3 @@
-// TODO: Make sure checking for errors everywhere
-
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -10,6 +8,7 @@ int main(int argc, char *argv[]) {
   int child1, child2, status;
   int fd[2]; // fd[0] is read end, fd[1] is write end
 
+  // Check for errors
   if(pipe(fd) < 0) {
     perror("pipe");
     return -1;
@@ -24,13 +23,15 @@ int main(int argc, char *argv[]) {
   }
   // ----CHILD1 CODE----
   else if(child1 == 0) {
-    close(fd[0]);
-    dup2(1, fd[1]);
-    execlp("/bin/ls", "ls", "-F", NULL);
-    pid_t child1_ppid = getppid();
-    // printf("child1_ppid = %d\n", child1_ppid);
+    int close_return1 = close(fd[0]); // Close read end
+    int dup2_return1 = dup2(fd[1], 1);
+    int execlp_return1 = execlp("ls", "ls", "-F", NULL);
+
+    if(close_return1 == 0 || dup2_return1 == 0 || execlp_return1 == 0) {
+      perror("error in child1");
+      return -1;
+    }
   }
-  // ----PARENT CODE----
   else {
     child2 = fork(); // Creating 2nd sibling
 
@@ -41,28 +42,32 @@ int main(int argc, char *argv[]) {
     }
     // ----CHILD2 CODE----
     else if(child2 == 0) {
-      close(fd[1]);
-      dup2(fd[0], 0);
-      execlp("/bin/nl", "nl", NULL);
-      pid_t child2_ppid = getppid();
-      // printf("child2_ppid = %d\n", child2_ppid);
+      int close_return2 = close(fd[1]); // Close write end
+      int dup2_return2 = dup2(fd[0], 0);
+      int execlp_return2 = execlp("nl", "nl", NULL);
+
+      if(close_return2 == 0 || dup2_return2 == 0 || execlp_return2 == 0) {
+        perror("error in child2");
+        return -1;
+      }
     }
     else {
-      // printf("parent_pid = %d\n", getpid());
+      // ----PARENT CODE----
+      // Check for errors
+      if(close(fd[1]) < 0) {
+        perror("close write end");
+        return -1;
+      }
+
+      // Parent process waiting for both children to execute
+      while(wait(&status) > 0) {
+        if(wait(NULL) < 0) {
+          perror("wait");
+          return -1;
+        }
+      }
+
+      return 0;
     }
   }
-
-  close(fd[0]);
-  close(fd[1]);
-
-  // Parent process waiting for both children to execute
-  while(wait(&status) > 0) {
-    if(wait(NULL) < 0) {
-      perror("wait");
-      return -1;
-    }
-    printf("Child complete");
-  }
-
-  return 0;
 }
