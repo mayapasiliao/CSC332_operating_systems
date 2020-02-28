a6b4321c6e0518d5ffb7c63f394053a132e74fbd
Maya Pasiliao <mayapasiliao@gmail.com>
2020-02-28 13:49:20 -0500

    Finished step 1 of part 3

---
+++ labs/task3_maya_pasiliao/part3/Process_P1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h> // for mode constants and create()
+
+int main(int argc, char *argv[]) {
+  int fd1 = creat("destination1.txt", S_IRWXU);
+  if(fd1 < 0) {
+    perror("creat1");
+  }
+
+    int fd2 = creat("destination2.txt", S_IRWXU);
+    if(fd2 < 0) {
+      perror("creat2");
+    }
+
+  return 0;
+}
