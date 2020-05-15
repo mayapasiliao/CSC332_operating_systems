d1235313dc8b84bc1a2ae4fb0dbe8c758291d9ed
Maya Pasiliao <mayapasiliao@gmail.com>
2020-05-15 18:05:05 -0400

    Finished assignment 3

--- assignments/maya_pasiliao_assignment3/bridges.c
+++ assignments/maya_pasiliao_assignment3/bridges.c
@@ -1,49 +1,46 @@
+// Compile with -pthread
+// This program assumes there can only be one farmer on the bridge at a time
 // wait -> semaphore is locked
 // signal -> semaphore is released
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t ok_to_cross = 1;
-sem_t mutex;
+sem_t ok_to_cross;
 
-// void enter_bridge();
-// void exit_bridge();
+void* cross_bridge(void* arg) {
+    // wait
+    sem_wait(&ok_to_cross);
+    printf("%s farmer can cross bridge\n", arg);
 
-void* thread(void* arg) {
-    //wait
-    sem_wait(&mutex);
-    printf("\nEntered..\n");
+    // critical section
+    int seconds = rand() % 4;
+    printf("%s is traveling on the bridge...\n", arg);
+    sleep(seconds);
 
-    //critical section
-    sleep(4);
+    // signal
+    printf("%s farmer has left the bridge\n\n", arg);
+    sem_post(&ok_to_cross);
 
-    //signal
-    printf("\nJust Exiting...\n");
-    sem_post(&mutex);
 }
 
 int main () {
-    sem_init(&mutex, 0, 1);
-    pthread_t t1,t2;
-    pthread_create(&t1,NULL,thread,NULL);
-    sleep(2);
-    pthread_create(&t2,NULL,thread,NULL);
-    pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
-    sem_destroy(&mutex);
+    // Initialize semaphore to 1
+    sem_init(&ok_to_cross, 0, 1);
+
+    pthread_t north1, south1, north2;
+    pthread_create(&north1, NULL, cross_bridge, "north1");
+    pthread_create(&south1, NULL, cross_bridge, "south1");
+    pthread_create(&north2, NULL, cross_bridge, "north2");
+
+    pthread_join(north1, NULL);
+    pthread_join(south1, NULL);
+    pthread_join(north2, NULL);
+
+    sem_destroy(&ok_to_cross);
 
     return 0;
 }
-
-// // wait
-// void enter_bridge() {
-//     sem_wait(ok_to_cross);
-//     printf()
-// }
-
-// // signal
-// void exit_bridge() {
-//     sem_post(ok_to_cross);
-// }
