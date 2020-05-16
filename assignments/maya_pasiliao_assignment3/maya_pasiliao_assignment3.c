87f19245ddc2828c14eb28a17a95188a8551c565
Maya Pasiliao <mayapasiliao@gmail.com>
2020-05-15 20:55:40 -0400

    Match to report

--- assignments/maya_pasiliao_assignment3/maya_pasiliao_assignment3.c
+++ assignments/maya_pasiliao_assignment3/maya_pasiliao_assignment3.c
@@ -31,14 +31,16 @@ int main () {
     // Initialize semaphore to 1
     sem_init(&ok_to_cross, 0, 1);
 
-    pthread_t north1, south1, north2;
+    pthread_t north1, south1, north2, south2;
     pthread_create(&north1, NULL, cross_bridge, "north1");
     pthread_create(&south1, NULL, cross_bridge, "south1");
     pthread_create(&north2, NULL, cross_bridge, "north2");
+    pthread_create(&south2, NULL, cross_bridge, "south2");
 
     pthread_join(north1, NULL);
     pthread_join(south1, NULL);
     pthread_join(north2, NULL);
+    pthread_join(south2, NULL);
 
     sem_destroy(&ok_to_cross);
 
